9efce689b2cd314ccc2241b57e7e2d1492fcc12f
Mateus Faria <mateusmecunhe@gmail.com>
2019-10-03 14:39:04 -0300

    adjusting pages structures

--- app.py
+++ app.py
@@ -17,35 +17,56 @@ class Todo(db.Model):
         return '<Task: %r>' % self.id 
 
 
-@app.route('/', methods=['POST', 'GET'])
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form['content']
+#         new_task = Todo(content=task_content)
+
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'error adding your task'
+
+#     else:
+#         #tasks = Todo.query.order_by(Todo.date_created).all()
+#         return render_template('index.html', tasks=tasks)
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
+    return render_template('index.html')
+
+@app.route('/task', methods=['POST', 'GET'])
+def task_master():
+    if request.method == "GET":
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('task-manager/task-master.html', tasks=tasks)
+    elif request.method == "POST":
         task_content = request.form['content']
         new_task = Todo(content=task_content)
 
         try:
             db.session.add(new_task)
             db.session.commit()
-            return redirect('/')
+            return redirect('/task')
         except:
             return 'error adding your task'
 
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-    
-@app.route('/delete/<int:id>')
+
+@app.route('/task/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
     
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        return redirect('/')
+        return redirect('/task')
     except:
         return 'error trying to delete'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@app.route('/task/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
@@ -54,12 +75,12 @@ def update(id):
 
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect('/task')
 
         except:
             return 'error while trying to update'
     else:
-        return render_template('update.html', task=task)
+        return render_template('task-manager/update.html', task=task)
 
 @app.route('/contact')
 def contact():
