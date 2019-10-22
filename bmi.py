4de5aaf229dbd9c03943f64bce6db3daa845bf20
Mateus Faria <mateusmecunhe@gmail.com>
2019-10-22 12:55:22 -0300

    restful api for bmi-calculator

--- bmi.py
+++ bmi.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, request, Response, redirect, Blueprint, flash
+from flask import Flask, render_template, url_for, request, Response, redirect, Blueprint, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import json
 bmi_bp = Blueprint('bmi_bp', __name__, url_prefix='/bmi')
 
 app = Flask(__name__)
@@ -20,9 +21,6 @@ def calculate_bmi(height, weight):
     height_in_m = height/100
     height_in_m_squared = height_in_m * height_in_m
     bmi = (weight/height_in_m_squared)
-    print(type(height))
-    print(type(weight))
-    print(bmi)
     return round(bmi, 2)
 
 def validate_pacient(pacient):
@@ -54,7 +52,6 @@ def bmi_calc():
         # flash(error)
         if len(error) is 0:
             try:
-                print('*a*a*A*A')
                 db.session.add(pacient)
                 db.session.commit()
                 return redirect('/bmi')
@@ -97,3 +94,72 @@ def update(id):
 
         return render_template('bmi-calc/update.html', pacient=pacient)
     
+@bmi_bp.route('/rest', methods=['POST', 'GET'])
+def json_data():
+    if request.method == 'POST':
+        pacient_posted = request.get_json()
+        pacient_posted_name = pacient_posted['name']
+        pacient_posted_height = float(pacient_posted['height'])
+        pacient_posted_weight = float(pacient_posted['weight'])
+        pacient_posted_bmi = calculate_bmi(height=pacient_posted_height, weight=pacient_posted_weight)
+
+        pacient = Pacient(name=pacient_posted_name, height=pacient_posted_height, weight=pacient_posted_weight, bmi=pacient_posted_bmi)
+        print(type(pacient))
+
+        pacient_dict = {}
+        pacient_dict["name"] = str(pacient.name)
+        pacient_dict["weight"] = str(pacient.weight)
+        pacient_dict["height"] = str(pacient.height)
+        pacient_dict["bmi"] = str(pacient.bmi)
+        print(pacient.id)
+    
+        print(type(pacient_dict))
+        print(pacient_dict)
+            
+        try:
+            db.session.add(pacient)
+            db.session.commit()
+            return json.dumps(pacient_dict)
+        except:
+            return 'error calculating your bmi'
+        return 'hey'
+    else:
+        pacients = Pacient.query.all()
+        pacients_list = []
+        for pacient in pacients:
+            pacient_dict = {}
+            pacient_dict["name"] = str(pacient.name)
+            pacient_dict["weight"] = str(pacient.weight)
+            pacient_dict["height"] = str(pacient.height)
+            pacient_dict["bmi"] = str(pacient.bmi)
+            pacients_list.append(pacient_dict)
+        pacients_json = json.dumps(pacients_list, sort_keys=True)
+        print(type(pacients_json))
+        print(json.loads(pacients_json))
+        return pacients_json
+
+
+@bmi_bp.route('/rest/<int:id>', methods=['PUT', 'DELETE'])
+def json_update_or_delete(id):
+    if request.method == 'PUT':
+        pacient = Pacient.query.get_or_404(id)
+        pacient_put = request.get_json()
+
+        pacient.name = pacient_put['name']
+        pacient.height = pacient_put['height']
+        pacient.weight = pacient_put['weight']
+        pacient.bmi = calculate_bmi(height=float(pacient.height), weight=float(pacient.weight))
+        try:
+            db.session.commit()
+        except:
+            "error trying to update"
+        return f'pacient {pacient.name} updated successfully'
+
+    elif request.method == "DELETE":
+        pacient_to_delete = Pacient.query.get_or_404(id)
+        try:
+            db.session.delete(pacient_to_delete)
+            db.session.commit()
+        except:
+            "error trying to delete"
+        return 'pacient deleted'
